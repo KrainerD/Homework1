f2a3ddca74ff583e0cc8f38a89fad4893fa5f78d
Diana Krainer <e11779458@student.tuwien.ac.at>
2024-03-10 00:12:45 +0100

    Add functions for electricity price, Bezugsgrößen and test

---
+++ electricity_price.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 23:04:26 2024
+
+@author: User
+"""
+import numpy as np
+
+def CRF(n, r):
+    return ((1+r)**n*r)/((1+r)**n-1)
+
+def fuel_costs(fuel_price, efficiency):
+    return fuel_price/efficiency
+
+def CO2_costs(CO2_price, emission_factor, efficiency):
+    return (CO2_price * emission_factor)/efficiency
+
+def electricity_prices(I, Power, operational_costs, fix_costs,  fuel_price, CO2_price, emission_factor, efficiency, n, r, T):
+    return (CRF(n, r)*I/Power + fix_costs)/T + operational_costs + CO2_costs(CO2_price, emission_factor, efficiency) + fuel_costs(fuel_price, efficiency)
+
+
+def Bezugsgroessen(U_N_eff,I_Str_N,f_N, T_R):
+    U_bez = np.sqrt(2/3) * U_N_eff
+    I_bez = np.sqrt(2) * I_Str_N
+    Z_bez = U_bez / I_bez
+    omega_el_bez = 2 * np.pi * f_N
+    T_bez = 1 / omega_el_bez
+    psi_bez = U_bez / omega_el_bez
+    tau_R = T_R / T_bez
+    return U_bez, I_bez, Z_bez, omega_el_bez, psi_bez, tau_R
+
+print(round(electricity_prices(1000000, 10, 1, 1, 50 , 50, 1.5 , 0.75, 15, 0.06, 3000),3))
---
+++ test_electricity_price.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  4 00:01:46 2024
+
+@author: User
+"""
+from electricity_price import CRF, fuel_costs, CO2_costs, electricity_prices, Bezugsgroessen
+
+def test_CRF():
+    assert round(CRF(15, 0.06),3) == 0.103
+
+def test_fuel_costs():
+    assert round(fuel_costs(50,0.75),3) == 66.667
+
+def test_CO2_costs():
+    assert CO2_costs(50, 1.5, 0.75) == 100
+
+def test_electricity_prices():
+    assert round(electricity_prices(1000000, 10, 1, 1, 50 , 50, 1.5 , 0.75, 15, 0.06, 3000),3) == 171.099
+
+def test_Bezugsgroessen():
+    assert round(Bezugsgroessen(460, 18, 60, 212.2*10**(-3))[0],2) == 375.59
+    assert round(Bezugsgroessen(460, 18, 60, 212.2*10**(-3))[1],2) == 25.46
+    assert round(Bezugsgroessen(460, 18, 60, 212.2*10**(-3))[2],2) == 14.75
+    assert round(Bezugsgroessen(460, 18, 60, 212.2*10**(-3))[3],2) == 376.99
+    assert round(Bezugsgroessen(460, 18, 60, 212.2*10**(-3))[4],2) == 1
+    assert round(Bezugsgroessen(460, 18, 60, 212.2*10**(-3))[5],2) == 80
+    
